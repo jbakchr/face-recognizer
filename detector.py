77054c04e885a8a693b55c33335388d41419d08c
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-12-10 09:35:36 +0100

    Added display face

--- detector.py
+++ detector.py
@@ -8,6 +8,8 @@ import face_recognition
 from PIL import Image, ImageDraw
 
 DEFAULT_ENCODINGS_PATH = Path("output/encodings.pkl")
+BOUNDING_BOX_COLOR = "blue"
+TEXT_COLOR = "white"
 
 Path("training").mkdir(exist_ok=True)
 Path("output").mkdir(exist_ok=True)
@@ -74,6 +76,22 @@ def _recognize_face(unknown_encoding, loaded_encodings):
     )
     if votes:
         return votes.most_common(1)[0][0]
+
+
+def _display_face(draw, bounding_box, name):
+    top, right, bottom, left = bounding_box
+    draw.rectangle(((left, top), (right, bottom)), outline=BOUNDING_BOX_COLOR)
+    text_left, text_top, text_right, text_bottom = draw.textbbox((left, bottom), name)
+    draw.rectangle(
+        ((text_left, text_top), (text_right, text_bottom)),
+        fill="blue",
+        outline="blue",
+    )
+    draw.text(
+        (text_left, text_top),
+        name,
+        fill="white",
+    )
 
 
 # encode_known_faces()
